63ebe6c1a819ad4888f4ac158096446d09d72fc2
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2023-03-05 19:13:22 +0800

    add initial value for useNode hook (for complex node event types)

--- packages/ui/src/hooks/useNode.ts
+++ packages/ui/src/hooks/useNode.ts
@@ -2,9 +2,9 @@ import { CreateSchema } from '@figmania/common'
 import { useEffect, useState } from 'react'
 import { useController } from './useController'
 
-export function useNode<S extends CreateSchema>(): S['events']['node:select'] {
-  const [node, setNode] = useState<S['events']['node:select']>()
+export function useNode<S extends CreateSchema>(initialNode?: S['events']['node:select']): S['events']['node:select'] {
+  const [node, setNode] = useState<S['events']['node:select']>(initialNode!)
   const controller = useController()
   useEffect(() => controller.addEventHandler('node:select', setNode), [])
-  return node!
+  return node
 }
