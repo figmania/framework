49638c9bf989c82e2d487cad7232232e4658535a
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2023-03-05 19:33:47 +0800

    Fix useConfig hook to remember config

--- packages/ui/src/hooks/useConfig.ts
+++ packages/ui/src/hooks/useConfig.ts
@@ -2,13 +2,21 @@ import { ConfigSchema } from '@figmania/common'
 import { useEffect, useState } from 'react'
 import { useController } from './useController'
 
+const lastConfig = {}
+
 export function useConfig<C>(): [C, (config: Partial<C>) => void] {
-  const [config, setConfig] = useState<C>({} as C)
+  const [config, setConfig] = useState<C>(lastConfig as C)
   const controller = useController<ConfigSchema<C>>()
   const saveConfig = (value: Partial<C>) => {
     controller.emit('config:save', value)
+    Object.assign(lastConfig, value)
     setConfig({ ...config, ...value })
   }
-  useEffect(() => controller.addEventHandler('config:changed', setConfig), [])
+  useEffect(() => {
+    controller.addEventHandler('config:changed', (value) => {
+      Object.assign(lastConfig, value)
+      setConfig(value)
+    })
+  }, [])
   return [config, saveConfig]
 }
