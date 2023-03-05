35136140a419441ae2d35d11238512ad9aa62dd7
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2023-03-05 17:48:34 +0800

    Fix common module nodePlugin typing

--- packages/common/src/plugins/nodePlugin.ts
+++ packages/common/src/plugins/nodePlugin.ts
@@ -3,7 +3,7 @@ import { CreateSchema } from '../messenger/Schema'
 
 export type NodeSchema = CreateSchema<{ events: { name: 'node:select', data: unknown } }>
 
-export function nodePlugin<S extends CreateSchema>(controller: Controller<S>, fn: (node: SceneNode) => S['events']['node:select']): void {
+export function nodePlugin<S extends CreateSchema>(controller: Controller<S>, fn: (node?: SceneNode) => S['events']['node:select']): void {
   const selectNode = () => {
     const [node] = figma.currentPage.selection
     const result = fn(node)
