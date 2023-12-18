613674ae1676bebe93c1f29c90e086bdb2cbfd7e
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2023-12-18 12:33:12 +0800

    ui: refactor useNode to useNodeEvent

--- packages/ui/src/hooks/useNodeEvent.ts
+++ packages/ui/src/hooks/useNodeEvent.ts
@@ -2,7 +2,7 @@ import { CreateSchema } from '@figmania/common'
 import { useEffect, useState } from 'react'
 import { useController } from './useFigma'
 
-export function useNode<S extends CreateSchema>(initialNode?: S['events']['node:select']): S['events']['node:select'] {
+export function useNodeEvent<S extends CreateSchema>(initialNode?: S['events']['node:select']): S['events']['node:select'] {
   const [node, setNode] = useState<S['events']['node:select']>(initialNode!)
   const controller = useController()
   useEffect(() => {
