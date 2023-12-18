318275fe8e8b6b9b8de68dab6a5c9f16f6cbcdda
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2023-12-18 12:32:52 +0800

    ui: Update Tabs to allow HTML attributes

--- packages/ui/src/components/Navigation/Tabs/Tabs.tsx
+++ packages/ui/src/components/Navigation/Tabs/Tabs.tsx
@@ -1,16 +1,16 @@
 import clsx from 'clsx'
-import { FunctionComponent, PropsWithChildren, ReactNode } from 'react'
+import { FunctionComponent, HTMLAttributes, PropsWithChildren, ReactNode } from 'react'
 import styles from './Tabs.module.scss'
 
-export interface TabsProps extends PropsWithChildren {
+export interface TabsProps extends PropsWithChildren, Omit<HTMLAttributes<HTMLDivElement>, 'onChange'> {
   items: ReactNode[]
   selectedIndex: number
   onChange: (index: number) => void
 }
 
-export const Tabs: FunctionComponent<TabsProps> = ({ items, selectedIndex, onChange, children }) => {
+export const Tabs: FunctionComponent<TabsProps> = ({ items, selectedIndex, onChange, className, children, ...props }) => {
   return (
-    <div className={styles['tabs']}>
+    <div className={clsx(styles['tabs'], className)} {...props}>
       {items.map((item, index) => (
         <div key={index} className={clsx(styles['tab'], index === selectedIndex && styles['active'])} onClick={() => {
           onChange(index)
