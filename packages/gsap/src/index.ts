cfdf28a5a55edefafbb0942224dca6a5d9f622c7
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2023-12-29 06:00:16 +0800

    gsap: Export Anim Ease Map / Selector

--- packages/gsap/src/index.ts
+++ packages/gsap/src/index.ts
@@ -2,19 +2,19 @@ import { AnimEase, AnimProperties } from '@figmania/common'
 import { gsap } from 'gsap'
 import { getAnimAttribute, getAnimSvgAttributes, getAnimTimelines } from './util/dom'
 
-const EaseAnimToGsap: Record<AnimEase, gsap.EaseString> = {
+export const EaseAnimToGsap: Record<AnimEase, gsap.EaseString> = {
   'linear': 'none',
   'ease-in': 'power1.in',
   'ease-out': 'power1.out',
   'ease-in-out': 'power1.inOut'
 }
 
-const ANIM_SELECTOR = AnimProperties.map((type) => `[anim\\:${type}]`).join(',')
+export const AnimSelector = AnimProperties.map((type) => `[anim\\:${type}]`).join(',')
 
 export function render(svg: SVGSVGElement) {
   const defaults = getAnimSvgAttributes(svg)
   const timeline = gsap.timeline({ defaults })
-  for (const element of Array.from(svg.querySelectorAll(ANIM_SELECTOR))) {
+  for (const element of Array.from(svg.querySelectorAll(AnimSelector))) {
     for (const { property, initialValue, transitions } of getAnimTimelines(element, defaults)) {
       transitions.forEach(({ from, to, value, ease }, index) => {
         if (index === 0) {
